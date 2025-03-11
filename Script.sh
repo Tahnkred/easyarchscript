b7a349bf3c2d8d80fb5c296cafda498250575bfb
Tahnkred <70665792+Tahnkred@users.noreply.github.com>
2025-03-11 15:36:11 +0100

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -56,10 +56,10 @@ echo -e ',512M,L\n,,L\n' | sfdisk ${DISK}
 # Creating variables for disk type names: NVMe or HDD/SSD
 
 if [[ ${DISK} =~ ^/dev/sd[a-z]$ ]]
-    then EFI= "${DISK}1" ; ROOT= "${DISK}2"
+    then EFI= ${DISK}1 ; ROOT= ${DISK}2
 
 elif [[ ${DISK} =~ ^/dev/nvme[0-9]+n1$ ]];
-    then EFI= "${DISK}p1" ; ROOT= "${DISK}p2"
+    then EFI= ${DISK}p1 ; ROOT= ${DISK}p2
 
 else echo -e "\e[31mError during partitioning, the disk type used is not recognized by the installation script. Installation process aborted.\e[0m"
      exit 0
