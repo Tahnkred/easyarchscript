e6e99e5a8bb1969171c739c2c7d92e0f746f4346
Tahnkred <70665792+Tahnkred@users.noreply.github.com>
2025-03-11 16:03:38 +0100

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -56,16 +56,16 @@ echo -e ',512M,L\n,,L\n' | sfdisk ${DISK}
 # Creating variables for disk type names: NVMe or HDD/SSD
 
 if [[ ${DISK} =~ ^/dev/sd[a-z]$ ]]
-    then $EFI="${DISK}1"
-         $ROOT="${DISK}2"
-    echo "\e[32mThe EFI partition has been created on ${EFI}.\e[0m"
-    echo "\e[32mThe ROOT partition has been created on ${ROOT}.\e[0m"
+    then EFI="${DISK}1"
+         ROOT="${DISK}2"
+    echo "\e[32mThe EFI partition has been created on\e[0m ${EFI}\e[32m.\e[0m"
+    echo "\e[32mThe ROOT partition has been created on\e[0m ${ROOT}\e[32m.\e[0m"
 
 elif [[ ${DISK} =~ ^/dev/nvme[0-9]+n1$ ]];
-    then $EFI="${DISK}p1"
-         $ROOT="${DISK}p2"
-    echo "\e[32mThe EFI partition has been created on ${EFI}.\e[0m"
-    echo "\e[32mThe ROOT partition has been created on ${ROOT}.\e[0m"
+    then EFI="${DISK}p1"
+         ROOT="${DISK}p2"
+    echo "\e[32mThe EFI partition has been created on\e[0m ${EFI}\e[32m.\e[0m"
+    echo "\e[32mThe ROOT partition has been created on\e[0m ${ROOT}\e[32m.\e[0m"
 
 else echo -e "\e[31mError during partitioning, the disk type used is not recognized by the installation script. Installation process aborted.\e[0m"
      exit 0
