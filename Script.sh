1b295290a38fb5e639a363478c8ae6085f93f1e4
Tahnkred <70665792+Tahnkred@users.noreply.github.com>
2025-03-12 14:57:38 +0100

    Update Script.sh

--- Script.sh
+++ Script.sh
@@ -63,7 +63,7 @@ parted --script ${DISK} mklabel gpt
 
 # Creating the EFI and ROOT partitions
 
-echo -e ',512M,L\n,,L' | sfdisk -f ${DISK}
+echo -e ',512M,L\n,,L' | sfdisk ${DISK} -f
 
 # Creating variables for disk type names: NVMe or HDD/SSD
 
