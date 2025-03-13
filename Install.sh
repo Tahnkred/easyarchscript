38a27fe0033ff502a59d5e1e80cba2d55a7ae71a
Tahnkred <70665792+Tahnkred@users.noreply.github.com>
2025-03-13 15:24:40 +0100

    creating another file for installation

--- Install.sh
+++ Install.sh
@@ -127,7 +127,6 @@ echo "Please select your time zone (format: Continent/Capital. Example: Europe/P
 
 #    #if [[ "${valid_timezones[@]}" = "${TIMEZONE}" ]];
 #     #   then    echo -e "\e[32mThe time zone has been set to "${TIMEZONE}".\e[0m"
-                timedatectl set-timezone "${TIMEZONE}"
 #      #  break
 
 #    elif ["${TIMEZONE}" = "Help" || "${TIMEZONE}" = "help" || "${TIMEZONE}" = "h" || "${TIMEZONE}" = "H"];
@@ -137,9 +136,6 @@ echo "Please select your time zone (format: Continent/Capital. Example: Europe/P
 #         echo -e "\e[1;32;1;4mHelp:\e[0m If you want to view the list of time zones, run the command '\e[32mtimedatectl list-timezones\e[0m'."
 #    fi
 #done
-
-# Enabling NTP synchronization
-timedatectl set-ntp true
 
 # Clear
 clear
@@ -153,114 +149,3 @@ echo
 echo
 echo "How would you like to name your main partition?"
 read ROOT_NAME
-
-# Formatting ${DISK}...
-wipefs --all ${DISK}
-lsblk
-# Creating the GPT partition table
-parted --script ${DISK} mklabel gpt
-
-# Creating the EFI and ROOT partitions
-
-echo -e ',512M,L\n,,L' | sfdisk ${DISK} -f
-
-# Creating variables for disk type names: NVMe or HDD/SSD
-
-if [[ ${DISK} =~ ^/dev/sd[a-z]$ ]]
-    then EFI="${DISK}1"
-         ROOT="${DISK}2"
-    echo -e "\e[32mThe EFI partition has been created on ${EFI}.\e[0m"
-    echo
-    echo -e "\e[32mThe ROOT partition has been created on ${ROOT}.\e[0m"
-
-elif [[ ${DISK} =~ ^/dev/nvme[0-9]+n1$ ]];
-    then EFI="${DISK}p1"
-         ROOT="${DISK}p2"
-    echo -e "\e[32mThe EFI partition has been created on ${EFI}.\e[0m"
-    echo
-    echo -e "\e[32mThe ROOT partition has been created on ${ROOT}.\e[0m"
-
-elif [[ ${DISK} =~ ^/dev/vd[a-z]$ ]];
-    then EFI="${DISK}1"
-         ROOT="${DISK}2"
-    echo -e "\e[32mThe EFI partition has been created on ${EFI}.\e[0m"
-    echo
-    echo -e "\e[32mThe ROOT partition has been created on ${ROOT}.\e[0m"
-
-else echo -e "\e[31mError during partitioning, the disk type used is not recognized by the installation script. Installation process aborted.\e[0m"
-     sleep 5s
-     exit 0
-fi
-
-# Clear
-clear
-
-# Formatting the EFI partition to FAT 32
-mkfs.vfat ${EFI}
-
-# Formatting the ROOT partition to Btrfs
-mkfs.btrfs -L ${ROOT_NAME} ${ROOT}
-
-# Clear
-clear
-
-# Generation of Btrfs subvolumes on ROOT
-echo "Partitioning of subvolumes ${ROOT}/mnt/@ & ${ROOT}/mnt/@home"
-mount ${ROOT} /mnt
-    btrfs su cr /mnt/@
-    btrfs su cr /mnt/@home
-umount /mnt
-
-# Mounting of ROOT partitions with the final parameters
-echo "Mounting of the ROOT partition"
-mount -o noatime,commit=120,compress=zstd,discard=async,space_cache=v2,subvol=@ ${ROOT} /mnt
-mount --mkdir -o noatime,commit=120,compress=zstd,discard=async,space_cache=v2,subvol=@home ${ROOT} /mnt/home
-
-# Mounting of EFI partition with the final parameters
-echo "Mounting of the EFI partition"
-mount --mkdir ${EFI} /mnt/efi
-
-# Clear
-clear
-
-# Regeneration of pacstrap keys
-echo "Regeneration of pacman keys"
-pacman-key -init
-pacman-key --populate
-pacman -Sy archlinux-keyring --noconfirm --needed
-
-# Clear
-clear
-
-# Installation of the base system
-echo "Installation of the base system"
-pacstrap -K /mnt base base-devel linux-zen linux-zen-headers linux-firmware intel-ucode amd-ucode btrfs-progs refind efibootmgr gptfdisk bash nano man-db tealdeer git mesa vulkan-radeon libva-mesa-driver mesa-vdpau --noconfirm --needed
-
-# Clear
-clear
-
-# Installation of the boot loader (refind.conf)
-echo "Installation of 'refind' (bootloader)"
-refind-install --root /mnt
-    sed -i 's/^timeout 20/timeout 3/' /mnt/efi/EFI/refind/refind.conf
-    sed -i 's/^#enable_mouse/enable_mouse/' /mnt/efi/EFI/refind/refind.conf
-    sed -i 's/^extra_kernel_version_strings linux-lts,linux/extra_kernel_version_strings linux-zen,linux-lts,linux-hardened,linux/' /mnt/efi/EFI/refind/refind.conf
-    sed -i 's/^#fold_linux_kernels false/fold_linux_kernels false/' /mnt/efi/EFI/refind/refind.conf
-    sed -i 's/^#default_selection "+,bzImage,vmlinuz"/default_selection "+,bzImage,vmlinuz"/' /mnt/efi/EFI/refind/refind.conf
-
-# Retrieving the UUID and modifying the boot parameters (refind_linux.conf)
-UUID=$(grep -oP 'UUID=\K[^\s]+' /mnt/boot/refind_linux.conf | head -n 1)
-cat /root/easyarchscript/Bootloader/refind_linux.conf > /mnt/boot/refind_linux.conf
-sed -i 's/(XXXXXXXX)/'${UUID}'/g' /mnt/boot/refind_linux.conf
-
-# Clear
-clear
-
-# Copying Chrooting.sh on /mnt /bin/bash in order to execute it
-cp /root/easyarchscript/Chrooting.sh /mnt/bin/bash
-
-# Sending values to Chrooting.sh in order to make it work
-/mnt/bin/bash/Chrooting.sh "${KEYBOARD}"
-
-#Chrooting
-arch-chroot /mnt /bin/bash sh Chrooting.sh
