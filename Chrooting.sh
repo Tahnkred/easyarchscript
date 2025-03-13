48769a6636c604dd53181e19eac58f14df8a5c68
Tahnkred <70665792+Tahnkred@users.noreply.github.com>
2025-03-13 23:27:09 +0100

    adding dns (I thing Chrooting worked)

--- Chrooting.sh
+++ Chrooting.sh
@@ -14,12 +14,12 @@ sed -i 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=15s/g' /etc/systemd/u
 sed -i 's/#Color/Color/g' /etc/pacman.conf
 sed -i 's/#VerbosePkgLists/VerbosePkgLists/g' /etc/pacman.conf
 sed -i 's/ParallelDownloads = 5/ParallelDownloads = 7/g' /etc/pacman.conf
-printf "ILoveCandy" | sudo tee -a /etc/pacman.conf --noconfirm --needed
+echo "ILoveCandy" >> /etc/pacman.conf
 sed -i 's/#[multilib]/[multilib]/g' /etc/pacman.conf
 sed -i 's/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlists/g' /etc/pacman.conf
 
 
 # Installing NetworkManager
-pacman -Syu networkmanager
+pacman -Syu networkmanager --noconfirm --needed
 systemctl enable NetworkManager
 
