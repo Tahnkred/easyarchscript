7ba5aa0e5277b88793454d729230dee188e723a2
Tahnkred <70665792+Tahnkred@users.noreply.github.com>
2025-03-13 23:16:47 +0100

    Update Chrooting.sh

--- Chrooting.sh
+++ Chrooting.sh
@@ -1,29 +1,25 @@
 #!/usr/bin/env bash
 
-printf "${CHROOT_KEYBOARD}"
-printf "test"
+printf "KEYMAP=%s\n" "$CHROOT_KEYBOARD"
 
-# Setting keyboard layout
-printf sudo loadkeys ${CHROOT_KEYBOARD}
+printf "KEYMAP=%s\n" "$CHROOT_KEYBOARD" >/etc/vconsole.conf
 
 # Making it permanent to the system
-sudo echo 'KEYMAP='${CHROOT_KEYBOARD}'' >> /etc/vconsole.conf
+printf "KEYMAP=%s\n" "$CHROOT_KEYBOARD" >/etc/vconsole.conf
 
 # Changing some timeout
-sudo sed -i 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=15s/g' /etc/systemd/user.conf
+sed -i 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=15s/g' /etc/systemd/user.conf
 
 # Pacman configuration
-sudo sed -i 's/#Color/Color/g' /etc/pacman.conf
-sudo sed -i 's/#VerbosePkgLists/VerbosePkgLists/g' /etc/pacman.conf
-sudo sed -i 's/ParallelDownloads = 5/ParallelDownloads = 7/g' /etc/pacman.conf
-sudo echo "ILoveCandy" | sudo tee -a /etc/pacman.conf
-sudo sed -i 's/#[multilib]/[multilib]/g' /etc/pacman.conf
-sudo sed -i 's/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlists/g' /etc/pacman.conf
+sed -i 's/#Color/Color/g' /etc/pacman.conf
+sed -i 's/#VerbosePkgLists/VerbosePkgLists/g' /etc/pacman.conf
+sed -i 's/ParallelDownloads = 5/ParallelDownloads = 7/g' /etc/pacman.conf
+printf "ILoveCandy" | sudo tee -a /etc/pacman.conf --noconfirm --needed
+sed -i 's/#[multilib]/[multilib]/g' /etc/pacman.conf
+sed -i 's/#Include = /etc/pacman.d/mirrorlist/Include = /etc/pacman.d/mirrorlists/g' /etc/pacman.conf
 
 
 # Installing NetworkManager
-sudo pacman -Syu networkmanager
-
-
-sudo systemctl enable NetworkManager
+pacman -Syu networkmanager
+systemctl enable NetworkManager
 
